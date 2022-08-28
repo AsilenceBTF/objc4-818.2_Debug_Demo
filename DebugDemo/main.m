f5f72dac2f2d138c6a1643f789522ec43863602c
asilencebtf <951208871@qq.com>
2022-08-28 20:39:00 +0800

    TBD

---
+++ DebugDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DebugDemo
+//
+//  Created by asilencebtf on 2022/8/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSArray *array = [[NSArray alloc] init];
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
