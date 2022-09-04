4b7ff9cf21e50f922164beb318e09214ecbce877
asilencebtf <951208871@qq.com>
2022-09-04 14:29:37 +0800

    finish native test

---
+++ TestDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by asilencebtf on 2022/9/3.
+//
+
+#import <Foundation/Foundation.h>
+#import "testController.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
